a55d37bc9dd6483e4f9a616c434bce953e348ae7
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-09-28 21:06:45 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите пятизначное число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void palindrome (int num)
+{  
+ int FifthDigit = num % 10;
+ int QuarterDigit = (num / 10) % 10;
+ int OneDigit = num / 10000;
+ int SecondDigit = (num / 1000) % 10;
+
+if (num > 9999 && num < 100000)
+{
+    if (OneDigit == FifthDigit)
+   {
+        if (SecondDigit == QuarterDigit)
+    {
+        Console.WriteLine ("Палиндром"); 
+        return; 
+    }
+   }
+    Console.WriteLine ("Непалиндром");
+    return; 
+}
+
+  Console.WriteLine ("Некорректное значение");
+
+
+}
+palindrome (num);
