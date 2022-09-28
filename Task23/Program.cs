45b5677cec9e75017afc2190d27941063747e03b
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-09-28 23:59:51 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+void qube(int agr)
+{
+    int i =1;
+    while (i<=agr)
+    {
+       Console.WriteLine($"{i} {Math.Pow((i),3)}"); 
+       i++;
+    }
+}
+qube(num);
